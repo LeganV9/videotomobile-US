3257ea5eff5e1753160a916e1b865050c31fc8a1
LeganV9 <yanisrenaud77@gmail.com>
2023-03-02 23:40:26 +0100

    Update add_car.php

--- add_car.php
+++ add_car.php
@@ -1,34 +1,31 @@
 <?php
-
 // Connexion à la base de données
-$host = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "database_name";
-$conn = new mysqli($host, $username, $password, $dbname);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+$db = new PDO('pgsql:host=nom_hote;port=5432;dbname=nom_base', 'nom_utilisateur', 'mot_de_passe');
 
-// Vérification des données du formulaire
-if (isset($_POST["make"]) && isset($_POST["model"]) && isset($_POST["year"]) && isset($_POST["price"]) && isset($_POST["image_url"]) && isset($_POST["description"])) {
-    $make = $_POST["make"];
-    $model = $_POST["model"];
-    $year = $_POST["year"];
-    $price = $_POST["price"];
-    $image_url = $_POST["image_url"];
-    $description = $_POST["description"];
+// Récupération des données envoyées par le formulaire
+$make = $_POST['make'];
+$model = $_POST['model'];
+$year = $_POST['year'];
+$price = $_POST['price'];
+$image_url = $_POST['image_url'];
+$description = $_POST['description'];
 
-    // Insertion de la nouvelle annonce dans la base de données
-    $sql = "INSERT INTO cars (make, model, year, price, image_url, description) VALUES ('$make', '$model', '$year', '$price', '$image_url', '$description')";
-    if ($conn->query($sql) === TRUE) {
-        header("Location: confirmation.php");
-        exit();
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-    }
-}
+// Préparation de la requête SQL d'insertion
+$query = "INSERT INTO cars (make, model, year, price, image_url, description) VALUES (:make, :model, :year, :price, :image_url, :description)";
+$stmt = $db->prepare($query);
 
-$conn->close();
+// Bind des valeurs pour éviter les injections SQL
+$stmt->bindParam(':make', $make);
+$stmt->bindParam(':model', $model);
+$stmt->bindParam(':year', $year);
+$stmt->bindParam(':price', $price);
+$stmt->bindParam(':image_url', $image_url);
+$stmt->bindParam(':description', $description);
 
+// Exécution de la requête d'insertion
+$stmt->execute();
+
+// Redirection vers la page d'accueil
+header('Location: index.html');
+exit();
 ?>
